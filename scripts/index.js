1b470c6491691566303fd834cfc5028be6cfa3b8
DimkaYmka <mungalov00@icloud.com>
2023-12-29 00:36:15 +0300

    review

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
+const cardsConteiner = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template');
 
-// @todo: DOM узлы
+const createNewCard = (name, link) => {
+    console.log(cardTemplate);
+    const newCard = cardTemplate.content.querySelector('.card').cloneNode(true);
 
-// @todo: Функция создания карточки
+    newCard.querySelector('.card__title').textContent = name;
+    const elementsImage = newCard.querySelector('.card__image');
+    elementsImage.src = link;
+    elementsImage.alt = name;
 
-// @todo: Функция удаления карточки
+    //удалениe карточки 
+    const deleteCard = newCard.querySelector('.card__delete-button');
+    deleteCard.addEventListener('click', () => {
+        newCard.remove();
+    });
+    return newCard;
+};
 
-// @todo: Вывести карточки на страницу
+const renderCard = (name, link) => {
+    cardsConteiner.append(createNewCard(name, link));
+};
+
+// вставляем карточки из массива
+initialCards.forEach((element) => {
+    renderCard(element.name, element.link);
+});
+
